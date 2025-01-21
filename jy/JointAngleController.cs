3236006d3be7aa262cbd8ed8a7accf643841a505
Jeong Yeon Lee <79257220+YeonYeonni@users.noreply.github.com>
2025-01-21 15:33:11 +0900

    Add files via upload

---
+++ jy/JointAngleController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class JointAngleController : MonoBehaviour
+{
+    public int frame;
+    public float position_x, position_y, position_z;
+    public float rotation_x, rotation_y, rotation_z;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+}
